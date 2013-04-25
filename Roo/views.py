9847c3e6676a6788bbae571e4874a7f8318cb8b7
Capella Yee <capellayee@gmail.com>
2013-04-25 11:22:43 -0400

    testing mybags.html

--- Roo/views.py
+++ Roo/views.py
@@ -82,6 +82,12 @@ def newbag():
 #  return render_template('neworderform.html')
 
 
+
+@app.route('/mybags/<userid>')
+def mybags(userid):
+  user = User.query.filter_by(id = userid).first()
+  return render_template('mybags.html', user=user)
+
 @app.route('/addtobag/<userid>', methods=['GET', 'POST'])
 def addtobag(userid):
     if request.method == 'POST':
@@ -90,7 +96,8 @@ def addtobag(userid):
         user = User.query.filter_by(id = userid).first()
         bag.users.append(user)
         db_session.commit()
-        return redirect(url_for('show_users'))
+        return redirect(url_for('addtobag'), userid=session.get('userid'))
+        #return redirect(url_for('show_users'))
     return render_template('addtobagform.html')
 
 @app.route('/remove/<userid>')
