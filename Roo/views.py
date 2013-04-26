171d152c83429fc9ae4ac8ddcca9f91849eeb7f4
Pranav Gokhale <pgokhale@princeton.edu>
2013-04-26 02:22:47 -0400

    gahhh testing email thing again

--- Roo/views.py
+++ Roo/views.py
@@ -158,14 +158,14 @@ def facebook_authorized(resp):
     session['facebook_token'] = (resp['access_token'], '')
 
     fbuser = facebook.get('me').data
-
-    if User.query.filter_by(email = fbuser['email']).first() == None:
-      user = User(fbuser['first_name'], fbuser['last_name'], fbuser['email'], '', '')
-      db_session.add(user)
-      db_session.commit()
+    return fbuser['email']
+#    if User.query.filter_by(email = fbuser['email']).first() == None:
+#      user = User(fbuser['first_name'], fbuser['last_name'], fbuser['email'], '', '')
+#      db_session.add(user)
+#      db_session.commit()
     
-    session['userid'] = User.query.filter_by(email = fbuser['email']).first().id
-    return redirect(url_for('home'))
+#    session['userid'] = User.query.filter_by(email = fbuser['email']).first().id
+#    return redirect(url_for('home'))
 
 @app.route("/logout")
 def logout():
