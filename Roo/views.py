c962e7b9557b84dd5f54b6afa9ec59c5254c89cf
Capella Yee <capellayee@gmail.com>
2013-05-06 15:39:45 -0400

    say no bags to display

--- Roo/views.py
+++ Roo/views.py
@@ -186,7 +186,7 @@ def mybags(userid):
   for bag in userbags:
     tuple = (Order.query.filter_by(bag_id=int(bag.id), user_id=userid).all(), str(bag.store))
     userorders.append(tuple)
-  return render_template('mybags.html', mybags=userbags, userorders=userorders, userid=user.id)
+  return render_template('mybags.html', mybags=userbags, userorders=userorders, userid=user.id, user=user)
 
 # confirmation page for removing an order
 @app.route('/removeorder/<orderid>')
